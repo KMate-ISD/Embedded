4bb570dd2a44ad9c9e3831376c1615ed9c963008
Krekó Imre Máté <mate.kreko@gmail.com>
2021-07-30 03:17:46 +0200

    Add solution

---
+++ Snake_A-1088BS_kmate/Snake_A-1088BS_kmate/main.c
@@ -0,0 +1,16 @@
+/*
+ * Snake_A-1088BS_kmate.c
+ *
+ * Created: 2021. 07. 29. 23:46:38
+ * Author : m8_krk
+ */ 
+
+#include <avr/io.h>
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1)
+    {
+    }
+}
