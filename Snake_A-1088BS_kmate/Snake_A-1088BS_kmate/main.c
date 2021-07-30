e85e4200409ffed8b02f2c69c17e43a0201b7ca1
Krekó Imre Máté <mate.kreko@gmail.com>
2021-07-30 03:19:45 +0200

    Add timer2 overflow

--- Snake_A-1088BS_kmate/Snake_A-1088BS_kmate/main.c
+++ Snake_A-1088BS_kmate/Snake_A-1088BS_kmate/main.c
@@ -6,6 +6,12 @@
  */ 
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
+
+unsigned char counter_byte = 0;
+
+void initialize_ports(void);
+void initialize_timer2_overflow(void);
 
 int main(void)
 {
@@ -14,3 +20,16 @@ int main(void)
     {
     }
 }
+
+void initialize_timer2_overflow()
+{
+	TCCR2A = 0;
+	TCCR2B = (1 << CS22) | (0 << CS21) | (1 << CS20);
+	TIMSK2 = (1 << TOIE2);
+	sei();
+}
+
+ISR(TIMER2_OVF_vect)
+{
+}
+
