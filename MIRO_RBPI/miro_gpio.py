1e382b2a665297ddcbef1a36a5fb23f910a1ef38
ISD-ikrk <mate.kreko@gmail.com>
2022-11-02 19:28:38 +0100

    Create gpio ancestor class

---
+++ MIRO_RBPI/miro_gpio.py
@@ -0,0 +1,40 @@
+import RPi.GPIO as GPIO
+
+class Miro_gpio:
+    def __init__(self, *pins, numbering, pin_mode) -> None:
+        self.numbering = numbering
+        self.pin_mode = pin_mode
+        self.pins = []
+        for pin in pins:
+            self.pins.append(pin)
+        GPIO.setmode(self.numbering)
+    
+    def __add_pin(self, pin):
+        self.pins.append(pin)
+
+    def setup(self, pin, pull_up_down=GPIO.PUD_OFF, initial=GPIO.UNKNOWN):
+        GPIO.setup(pin, self.pin_mode, pull_up_down=pull_up_down, initial=initial)
+
+
+class Miro_input(Miro_gpio):
+    def __init__(self, *pins, numbering=GPIO.BOARD) -> None:
+        super().__init__(*pins, numbering=numbering, pin_mode=GPIO.IN)
+        self.callbacks = []
+    
+    def setup_all(self, pull_up_down=GPIO.PUD_DOWN):
+        for pin in self.pins:
+            self.setup(pin, pull_up_down=pull_up_down)
+    
+    def add_input(self, pin, setup=0):
+        self.__add_pin(pin)
+        if setup:
+            self.setup(pin, pull_up_down=GPIO.PUD_DOWN)
+    
+
+class Miro_output(Miro_gpio):
+    def __init__(self, *pins, numbering=GPIO.BOARD) -> None:
+        super().__init__(*pins, numbering=numbering, pin_mode=GPIO.OUT)
+    
+    def setup_all(self, initial=GPIO.LOW):
+        for pin in self.pins:
+            super().__setup(pin, initial=initial)
