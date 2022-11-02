4597f082a8d1403c85ad03d75706408fe570cf16
ISD-ikrk <mate.kreko@gmail.com>
2022-11-02 05:34:21 +0100

    Add main for testing

--- MIRO_RBPI/miro_led.py
+++ MIRO_RBPI/miro_led.py
@@ -75,3 +75,48 @@ class miro_led:
         self.red_off()
         self.green_off()
         self.blue_off()
+
+try:
+    if __name__ == "__main__":
+        params = [
+            "-L", # "--leds"
+            "-r", # "--red"
+            "-g", # "--green"
+            "-b", # "--blue"
+            "-s"] # "--sleep"
+
+        args = sys.argv[:]
+        args.pop(0)
+        flags = args[::2]
+        args = args[1::2]
+        cla = dict(zip(flags, args))
+        print(f"Arguments count: {len(cla)}")
+
+        for flag in params:
+            if flag in cla:
+                if flag == "-L":
+                    cla[flag] = [int(i, 16) for i in cla[flag]]
+                elif flag == "-s":
+                    cla[flag] = float(cla[flag])
+                else:
+                    cla[flag] = int(cla[flag])
+        print(cla)
+
+        leds = cla["-L"]
+        rest = cla["-s"]
+
+        led = miro_led(leds)
+
+        led.red_flash(rest)
+        led.green_flash(rest)
+        led.blue_flash(rest)
+
+        led.red_pulse()
+        led.green_pulse()
+        led.blue_pulse()
+
+except Exception as e:
+    print(f"{e}")
+finally:
+    GPIO.cleanup()
+    print("Exiting script.")
