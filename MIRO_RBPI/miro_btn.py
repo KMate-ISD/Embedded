daf2dac4ef587131b3a5d5a54f70adbe579c1c6b
ISD-ikrk <mate.kreko@gmail.com>
2022-11-03 01:52:44 +0100

    Add pull-up/down parameter to constructor

--- MIRO_RBPI/miro_btn.py
+++ MIRO_RBPI/miro_btn.py
@@ -1,11 +1,12 @@
 import RPi.GPIO as GPIO
 from miro_gpio import Miro_input
+from miro_rgb import Miro_rgb
 
 class Miro_btn(Miro_input):
-    def __init__(self, *pins, numbering=GPIO.BOARD, setup=1) -> None:
+    def __init__(self, *pins, numbering=GPIO.BOARD, pull_up_down=GPIO.PUD_DOWN, setup=1) -> None:
         super().__init__(*pins, numbering=numbering)
         if setup and pins:
-            self.setup_all()
+            self.setup_all(pull_up_down)
     
     def add_handler(self, pin, event, *args):
         for callback in args:
@@ -19,12 +20,16 @@ class Miro_btn(Miro_input):
 try:
     if __name__ == "__main__":
         pin = 36
+        btn = Miro_btn(pin)
+        rgb = Miro_rgb(11, 13, 15)
         def cb(channel):
             print("Button pressed")
-        btn = Miro_btn(pin)
         btn.add_handler(pin, GPIO.RISING, cb)
-        exit = input("Press 'Enter' to remove handler (print)...\n")
+        btn.add_handler(pin, GPIO.RISING, rgb.blue_flash)
+        
+        exit = input("Press 'Enter' to remove handlers...\n")
         btn.remove_handlers(pin)
+
         exit = input("Press 'Enter' to exit...\n")
 
 except Exception as e:
