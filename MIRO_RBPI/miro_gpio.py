87d5bef616e88e0ca0db1fd6e46ff8d94d3bf46d
ISD-ikrk <mate.kreko@gmail.com>
2022-11-03 01:39:41 +0100

    Update gpio setup

--- MIRO_RBPI/miro_gpio.py
+++ MIRO_RBPI/miro_gpio.py
@@ -8,9 +8,6 @@ class Miro_gpio:
         for pin in pins:
             self.pins.append(pin)
         GPIO.setmode(self.numbering)
-    
-    def __add_pin(self, pin):
-        self.pins.append(pin)
 
     def setup(self, pin, pull_up_down=GPIO.PUD_OFF, initial=GPIO.UNKNOWN):
         GPIO.setup(pin, self.pin_mode, pull_up_down=pull_up_down, initial=initial)
@@ -21,23 +18,23 @@ class Miro_input(Miro_gpio):
         super().__init__(*pins, numbering=numbering, pin_mode=GPIO.IN)
         self.callbacks = []
     
-    def setup_all(self, pull_up_down=GPIO.PUD_DOWN):
+    def setup_all(self, pull_up_down):
         for pin in self.pins:
             self.setup(pin, pull_up_down=pull_up_down)
     
-    def add_input(self, pin, setup=0):
-        self.__add_pin(pin)
-        if setup:
-            self.setup(pin, pull_up_down=GPIO.PUD_DOWN)
-    
 
 class Miro_output(Miro_gpio):
-    def __init__(self, *pins, numbering) -> None:
+    def __init__(self, *pins, numbering, initial) -> None:
         super().__init__(*pins, numbering=numbering, pin_mode=GPIO.OUT)
+        self.statuses = [initial]*len(self.pins)
+
+    def setup(self, pin, initial):
+        super().setup(pin, initial=initial)
+        self.statuses[self.pins.index(pin)] = initial
     
-    def setup_all(self, initial=GPIO.LOW):
+    def setup_all(self, initial):
         for pin in self.pins:
-            super().setup(pin, initial=initial)
+            self.setup(pin, initial=initial)
 
     def set_pin(self, pin):
         GPIO.output(pin, GPIO.HIGH)
