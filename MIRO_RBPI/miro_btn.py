5a67707ad7dc33734f90d293dd8325c3c81e9913
ISD-ikrk <mate.kreko@gmail.com>
2022-11-03 02:36:21 +0100

    Update to split button and demo

--- MIRO_RBPI/miro_btn.py
+++ MIRO_RBPI/miro_btn.py
@@ -1,6 +1,5 @@
 import RPi.GPIO as GPIO
 from miro_gpio import Miro_input
-from miro_rgb import Miro_rgb
 
 class Miro_btn(Miro_input):
     def __init__(self, *pins, numbering=GPIO.BOARD, pull_up_down=GPIO.PUD_DOWN, setup=1) -> None:
@@ -16,24 +15,3 @@ class Miro_btn(Miro_input):
     def remove_handlers(self, pin):
         self.callbacks.clear()
         GPIO.remove_event_detect(pin)
-
-try:
-    if __name__ == "__main__":
-        pin = 36
-        btn = Miro_btn(pin)
-        rgb = Miro_rgb(11, 13, 15)
-        def cb(channel):
-            print("Button pressed")
-            rgb.blue_flash()
-        btn.add_handler(pin, GPIO.RISING, cb)
-        
-        exit = input("Press 'Enter' to remove handlers...\n")
-        btn.remove_handlers(pin)
-
-        exit = input("Press 'Enter' to exit...\n")
-
-except Exception as e:
-    print(f"{e}")
-finally:
-    GPIO.cleanup()
-    print("Exiting script.")
---
+++ MIRO_RBPI/miro_btn_demo.py
@@ -0,0 +1,24 @@
+import RPi.GPIO as GPIO
+from miro_btn import Miro_btn
+from miro_rgb import Miro_rgb
+
+try:
+    if __name__ == "__main__":
+        pin = 36
+        btn = Miro_btn(pin)
+        rgb = Miro_rgb(11, 13, 15)
+        def cb(channel):
+            print("Button pressed")
+            rgb.blue_pulse(0.125)
+        btn.add_handler(pin, GPIO.RISING, cb)
+        
+        exit = input("Press 'Enter' to remove handlers...\n")
+        btn.remove_handlers(pin)
+
+        exit = input("Press 'Enter' to exit...\n")
+
+except Exception as e:
+    print(f"{e}")
+finally:
+    GPIO.cleanup()
+    print("Exiting script.")
