284eccb473fe2f59ee0c811cd4a632446f8a9522
ISD-ikrk <mate.kreko@gmail.com>
2022-11-03 01:40:06 +0100

    Create miro_led.py

---
+++ MIRO_RBPI/miro_led.py
@@ -0,0 +1,32 @@
+import RPi.GPIO as GPIO
+from miro_gpio import Miro_output
+from time import sleep
+
+class Miro_led(Miro_output):
+    def __init__(self, *pins, numbering=GPIO.BOARD, initial=GPIO.LOW) -> None:
+        super().__init__(*pins, numbering=numbering, initial=initial)
+        self.setup_all(initial)
+
+    def __led_toggle(self, led_id):
+        self.statuses[led_id] = not self.statuses[led_id]
+        GPIO.output(self.pins[0], self.statuses[1])
+
+    def __led_flash(self, led_id, rest, cycles, off=False):
+        for i in range(cycles):
+            self.__led_toggle(led_id)
+            sleep(rest)
+        if off:
+            self.__led_off(led_id)
+
+    def __led_pulse(self, led_id, rest):
+        self.__led_toggle(led_id)
+        sleep(rest)
+        self.__led_toggle(led_id)
+    
+    def __led_off(self, led_id):
+        self.statuses[led_id] = 0
+        self.clear_pin(self.pins[led_id])
+    
+    def __led_on(self, led_id):
+        self.statuses[led_id] = 1
+        self.set_pin(self.pins[led_id])
