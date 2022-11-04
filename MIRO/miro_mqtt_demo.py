d9999e096572743417f244dffd26bb2763f3f0ea
ISD-ikrk <mate.kreko@gmail.com>
2022-11-04 04:30:33 +0100

    Update demo script

--- MIRO/miro_mqtt_demo.py
+++ MIRO/miro_mqtt_demo.py
@@ -1,11 +1,11 @@
-from src.rbpi_mqtt.miro_mqtt_client_catalogue import Miro_mqtt_client_catalogue
+from src.rbpi_mqtt.miro_mqtt_client_handler import Miro_mqtt_client_handler
 
 try:
-    mr = Miro_mqtt_client_catalogue("192.168.1.85", 1883, "admin/debug", "auth/user")
+    mr = Miro_mqtt_client_handler("192.168.1.85", 1883, "admin/debug", "auth/user")
     mr.add_client(kmate_mqtt="lorimmer_mqtt")
     mr.clients["kmate_mqtt"].on_connect = mr.on_connect
     mr.clients["kmate_mqtt"].on_message = mr.on_message
-    mr.connect_client("kmate_mqtt")
+    mr.connect("kmate_mqtt")
     for topic in mr.topics:
         mr.clients["kmate_mqtt"].subscribe(topic)
     mr.clients["kmate_mqtt"].loop_start()
