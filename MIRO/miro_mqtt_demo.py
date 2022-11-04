61a86379a1c5f7893e3cf1576c49c08963c61e52
ISD-ikrk <mate.kreko@gmail.com>
2022-11-04 09:58:58 +0100

    Update indexing

--- MIRO/miro_mqtt_demo.py
+++ MIRO/miro_mqtt_demo.py
@@ -1,17 +1,6 @@
 from src.rbpi_mqtt.miro_mqtt_action_handler import Miro_mqtt_action_handler
 
 try:
-    mc = Miro_mqtt_action_handler("192.168.1.85", 1883, "admin/debug", "auth/user")
-    mc.add_client(kmate_mqtt="lorimmer_mqtt")
-    mc.register_on_connect(mc.on_connect)
-    mc.register_on_message(mc.on_message)
-    mc.register_on_disconnect(mc.on_disconnect)
-    mc.connect()
-    for topic in mc.topics:
-        mc.subscribe(topic)
-    mc.start()
-    input("Press ENTER to add next client...")
-
     mx = Miro_mqtt_action_handler("192.168.1.85", 1883, "admin/debug", "auth/user")
     creds = mx.generate_credentials(12)
     mx.save_credentials(creds)
--- MIRO/src/rbpi_mqtt/miro_mqtt_action_handler.py
+++ MIRO/src/rbpi_mqtt/miro_mqtt_action_handler.py
@@ -20,16 +20,16 @@ class Miro_mqtt_action_handler(Miro_mqtt_client_handler):
 
         pw = list()
         for i in range(i):
-            pw.append(chr(random.randint(32, 128)))
+            pw.append(chr(random.randint(32, 127)))
 
         return(user, ''.join(pw))
     
     def generate_username(self, i=4):
         user = list()
         for i in range(i):
-            r = random.randint(48, 123)
+            r = random.randint(48, 122)
             while (r < 65 and r > 57) or (r < 97 and r > 90):
-                r = random.randint(48, 123)
+                r = random.randint(48, 122)
             user.append(chr(r))
 
         return(''.join(user))
