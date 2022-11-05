9c760ae2d7fee4dff2a76d4a64c6ff9daab374ff
ISD-ikrk <mate.kreko@gmail.com>
2022-11-05 09:33:54 +0100

    Add to keep track of events too

--- MIRO/src/rbpi_gpio/miro_btn.py
+++ MIRO/src/rbpi_gpio/miro_btn.py
@@ -4,12 +4,14 @@ from src.rbpi_gpio.miro_gpio import Miro_input
 class Miro_btn(Miro_input):
     def __init__(self, *pins, numbering=GPIO.BOARD, pull_up_down=GPIO.PUD_DOWN, setup=1) -> None:
         super().__init__(*pins, numbering=numbering)
+        self.events = list()
         if setup and pins:
             self.setup_all(pull_up_down)
     
     def add_handler(self, pin, event, *args):
         for callback in args:
             self.callbacks.append(callback)
+            self.events.append(event)
             GPIO.add_event_detect(pin, event, callback=callback)
     
     def remove_handlers(self, pin):
