81039f61db6adf245d0a32ceb63d3905d52ca06e
ISD-ikrk <mate.kreko@gmail.com>
2022-11-09 02:56:19 +0100

    Add NVM handling

--- MIRO/src/mcu_state_machine/mcu_state_machine.ino
+++ MIRO/src/mcu_state_machine/mcu_state_machine.ino
@@ -12,20 +12,21 @@
 #include <Preferences.h>
 #include <PubSubClient.h>
 #include <WiFi.h>
+#include <cstdarg>
 
 
 /*
  * MACRO
  */
 
-#define BTN             0
-#define LED             2
-#define RF_RST          22
-#define RF_SS           21
-#define TIMER0          0
-#define TIMER1          1
+#define BTN                   0
+#define LED                   2
+#define RF_RST                22
+#define RF_SS                 21
+#define TIMER0                0
+#define TIMER1                1
 
-#define DEBUG(a)        if (debug) { a; }
+#define DEBUG(a)              if (debug) { a; }
 
 
 /*
@@ -55,13 +56,12 @@ enum Switch_state
  */
 
   // const
-const size_t BAUD_RATE  = 9600;
-const size_t REST       = 6000;
-const size_t TIMER_DIV  = 80;
-const size_t TIMER_TRSH = 500000;
-
-  // NVM
-Preferences preferences;
+const uint8_t LEN_MQ_CREDS  = 4;
+const uint8_t LEN_MAX_IP    = 16;
+const size_t BAUD_RATE      = 9600;
+const size_t REST           = 6000;
+const size_t TIMER_DIV      = 80;
+const size_t TIMER_TRSH     = 500000;
 
   // Wi-Fi
 const char* wlan_ssid;
@@ -76,24 +76,39 @@ const char* mqtt_user;
 PubSubClient* mqtt_client;
 
   // Op.
-bool held               = false;
-bool debug              = true;
-uint8_t miro_state      = Undefined;
-size_t t0;
-size_t td               = 0;
+bool held                   = false;
+bool debug                  = true;
+uint8_t miro_state          = Undefined;
+size_t t0                   = 0;
+size_t td;
 
   // Peripherals
-bool led_state          = false;
-uint8_t switch_state    = sw_normal;
+bool led_state              = false;
+uint8_t switch_state        = sw_normal;
 
   // Timer
-bool autoreload         = true;
-bool edge               = true;
-bool timer_count_up     = true;
-uint64_t alarm_treshold = TIMER_TRSH;
-uint16_t timer_divider  = TIMER_DIV;
+bool autoreload             = true;
+bool edge                   = true;
+bool timer_count_up         = true;
+uint64_t alarm_treshold     = TIMER_TRSH;
+uint16_t timer_divider      = TIMER_DIV;
 hw_timer_t* timer_cycle;
 hw_timer_t* timer_span;
+
+  // NVM
+Preferences preferences;
+
+  // Credentials
+typedef struct {
+  size_t mqtt_port;
+  uint8_t* mqtt_broker;
+  char* mqtt_user;
+  char* mqtt_pass;
+  char* wlan_psk;
+  char* wlan_ssid;
+  uint8_t len_wlan_psk;
+  uint8_t len_wlan_ssid;
+} Credentials_t;
 
 
 /*
@@ -117,6 +132,20 @@ void IRAM_ATTR on_alarm_span(void);
  * STATE 0, 1
  */
 
+uint8_t check_if_preferences_has_keys(uint8_t args_count, ...)
+{
+  va_list args;
+  uint8_t i;
+  uint8_t log_val;
+  va_start(args, args_count);
+  for (i = 0; i < args_count; i++)
+  {
+    log_val += preferences.isKey(va_arg(args, const char*));
+  }
+  va_end(args);
+  return(log_val);
+}
+
 void setup()
 {
   // [0] Initialize
@@ -126,7 +155,67 @@ void setup()
   DEBUG(Serial.println())
 
     // NVM
+  Credentials_t creds;
 
+  preferences.begin("miro_creds", true);
+  bool exist_port = preferences.isKey("mqtt_port");
+  bool exist_broker = preferences.isKey("mqtt_broker");
+  bool exist_user = preferences.isKey("mqtt_user");
+  bool exist_pass = preferences.isKey("mqtt_pass");
+  bool exist_psk = preferences.isKey("wlan_psk");
+  bool exist_ssid = preferences.isKey("wlan_ssid");
+  bool exist = (
+    exist_port
+    && exist_broker
+    && exist_user
+    && exist_pass
+    && exist_psk
+    && exist_ssid);
+
+  bool exist2 = check_if_preferences_has_keys(6, "mqtt_port", "mqtt_broker", "mqtt_user", "mqtt_pass", "wlan_psk", "wlan_ssid");
+  Serial.print("exist2: ");
+  Serial.println(exist2);
+
+  if (exist)
+  {
+    creds.len_wlan_psk = preferences.getUChar("len_psk");
+    creds.len_wlan_ssid = preferences.getUChar("len_ssid");
+
+    const char* broker_str = (char*)malloc((LEN_MAX_IP + 1)*sizeof(char));
+    creds.mqtt_broker = (uint8_t*)malloc(LEN_MQ_CREDS*sizeof(uint8_t));
+    creds.mqtt_user = (char*)malloc((LEN_MQ_CREDS + 1)*sizeof(char));
+    creds.mqtt_pass = (char*)malloc((LEN_MQ_CREDS + 1)*sizeof(char));
+    creds.wlan_psk = (char*)malloc((creds.len_wlan_psk)*sizeof(char));
+    creds.wlan_ssid = (char*)malloc((creds.len_wlan_ssid)*sizeof(char));
+
+    creds.mqtt_port = preferences.getUShort("mqtt_port");
+    preferences.getBytes("mqtt_broker", creds.mqtt_broker, LEN_MAX_IP/4);
+    parse_ip_to_string(broker_str, creds.mqtt_broker);
+    preferences.getString("mqtt_user", creds.mqtt_user, LEN_MQ_CREDS + 1);
+    preferences.getString("mqtt_pass", creds.mqtt_pass, LEN_MQ_CREDS + 1);
+    preferences.getString("wlan_psk", creds.wlan_psk, creds.len_wlan_psk);
+    preferences.getString("wlan_ssid", creds.wlan_ssid, creds.len_wlan_ssid);
+
+    Serial.println("Network configuration loaded from non-volatile memory.");
+    Serial.print("Port ");
+    Serial.println(creds.mqtt_port);
+    Serial.print("Broker ");
+    Serial.println(broker_str);
+    Serial.print("User ");
+    Serial.println(creds.mqtt_user);
+    Serial.print("Pass ");
+    Serial.println(creds.mqtt_pass);
+    Serial.print("Psk ");
+    Serial.println(creds.wlan_psk);
+    Serial.print("Ssid ");
+    Serial.println(creds.wlan_ssid);
+  }
+  else
+  {
+    Serial.println("No existing network configuration found.");
+  }
+  
+  preferences.end();
 
     // WLAN
   wlan_ssid   = "Telekom-B4Wf5Y";
@@ -137,10 +226,27 @@ void setup()
   mqtt_port   = 1883;
   mqtt_user   = "user";
   mqtt_pass   = "pass";
-  mqtt_broker = (const char*)malloc(16*sizeof(char*));
-  uint8_t mqtt_broker_bytes[4] = {192, 168, 1, 85};
+  mqtt_broker = (const char*)malloc(LEN_MAX_IP*sizeof(char));
+  uint8_t mqtt_broker_bytes[LEN_MAX_IP/4] = {192, 168, 1, 85};
   parse_ip_to_string(mqtt_broker, mqtt_broker_bytes);
   init_mqtt();
+
+    // Creds
+  if (1 || !exist)
+  {
+    preferences.begin("miro_creds", false);
+    Serial.print("Writing stuff to NVM. ");
+    preferences.putUChar("len_psk", 13 + 1);
+    preferences.putUChar("len_ssid", 14 + 1);
+    preferences.putUShort("mqtt_port", mqtt_port);
+    preferences.putBytes("mqtt_broker", mqtt_broker_bytes, LEN_MAX_IP/4);
+    preferences.putString("mqtt_user", mqtt_user);
+    preferences.putString("mqtt_pass", mqtt_pass);
+    preferences.putString("wlan_psk", wlan_psk);
+    preferences.putString("wlan_ssid", wlan_ssid);
+    preferences.end();
+    Serial.println("Done.");
+  }
 
     // Peripherals
   pinMode(LED, OUTPUT);
@@ -172,8 +278,8 @@ void loop()
       if (!mqtt_client->connected()) { mqtt_reconnect(); }
       mqtt_client->loop();
 
-      td = t0 = millis();
-      if (td - t0 > REST*5)
+      td = millis();
+      if (td - t0 > REST*4)
       {
         t0 = td;
         Serial.print("state ");
@@ -208,9 +314,10 @@ void init_wifi()
   
   while (WiFi.status() != WL_CONNECTED)
   {
-    DEBUG(Serial.print('.'));
+    DEBUG(Serial.print('.'))
     delay(REST/6);
   }
+  DEBUG(Serial.print(' '))
   
   DEBUG(Serial.println(WiFi.localIP()))
 }
